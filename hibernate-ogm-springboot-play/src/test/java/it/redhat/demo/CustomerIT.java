b89765c9728d1175d1fc52e7d7b5dad004b671a1
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-11-26 10:40:28 +0100

    Spring Boot and Hibernate OGM sample

---
+++ hibernate-ogm-springboot-play/src/test/java/it/redhat/demo/CustomerIT.java
@@ -0,0 +1,28 @@
+package it.redhat.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerIT {
+
+	@Autowired
+	private CustomerNoSQLRepository repo;
+
+	@Test
+	public void gogogo() {
+		List<Customer> all = repo.findAll();
+		assertEquals( Collections.emptyList(), all );
+	}
+
+}
