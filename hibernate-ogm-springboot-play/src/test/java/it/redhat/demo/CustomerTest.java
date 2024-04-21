2521f53f364f172521e6e6e1d8e3fd6d102a143b
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-04-21 07:52:01 +0200

    Upgrade the project using Java 11

--- hibernate-ogm-springboot-play/src/test/java/it/redhat/demo/CustomerTest.java
+++ hibernate-ogm-springboot-play/src/test/java/it/redhat/demo/CustomerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerIT {
+public class CustomerTest {
 
 	@Autowired
 	private CustomerNoSQLRepository repo;
